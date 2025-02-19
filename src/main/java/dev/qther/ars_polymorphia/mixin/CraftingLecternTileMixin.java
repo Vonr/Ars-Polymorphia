6f849f74c4b64d2120273cc46e534ad0cc0abb71
Qther <qther@tuta.io>
2025-02-19 16:17:31 +0800

    fix: lectern crafting not working on dedicated servers

--- src/main/java/dev/qther/ars_polymorphia/mixin/CraftingLecternTileMixin.java
+++ src/main/java/dev/qther/ars_polymorphia/mixin/CraftingLecternTileMixin.java
@@ -3,10 +3,12 @@ package dev.qther.ars_polymorphia.mixin;
 import com.hollingsworth.arsnouveau.common.block.tile.CraftingLecternTile;
 import com.hollingsworth.arsnouveau.common.block.tile.TransientCustomContainer;
 import com.illusivesoulworks.polymorph.api.common.base.IRecipePair;
-import com.illusivesoulworks.polymorph.client.RecipesWidget;
 import com.illusivesoulworks.polymorph.common.util.RecipePair;
+import dev.qther.ars_polymorphia.packets.serverbound.PacketSetRecipe;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,13 +16,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Mixin(CraftingLecternTile.class)
 public abstract class CraftingLecternTileMixin {
     @Shadow public abstract TransientCustomContainer getCraftingInv(UUID uuid);
+
+    @Shadow private CraftingRecipe currentRecipe;
 
     @SuppressWarnings("resource")
     @Inject(method = "onCraftingMatrixChanged", at = @At("HEAD"))
@@ -40,20 +44,14 @@ public abstract class CraftingLecternTileMixin {
             return;
         }
 
-        var maybeWidget = RecipesWidget.get();
-        if (maybeWidget.isEmpty()) {
-            return;
-        }
-
         var recipes = serverLevel.getRecipeManager().getRecipesFor(RecipeType.CRAFTING, this.getCraftingInv(uuid).asCraftInput(), serverLevel);
 
-        Set<IRecipePair> set = new HashSet<>();
+        List<IRecipePair> pairs = new ArrayList<>();
 
         for (var recipe : recipes) {
-            set.add(new RecipePair(recipe.id(), recipe.value().getResultItem(level.registryAccess())));
+            pairs.add(new RecipePair(recipe.id(), recipe.value().getResultItem(level.registryAccess())));
         }
 
-        var widget = maybeWidget.get();
-        widget.setRecipesList(set, null);
+        PacketDistributor.sendToPlayer(player, new PacketSetRecipe(pairs));
     }
 }
